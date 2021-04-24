ab38feee358f0ff16af78eb147cfef3ed4814fef
JSound-ПК\J.Sound <igor2d@mail.ru>
2021-04-24 22:35:07 +0300

    24.04

--- BlazorTutorial/EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ BlazorTutorial/EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -13,66 +13,81 @@ namespace EmployeeManagement.Web.Pages
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
 
+        public bool ShowFooter { get; set; } = true;
+
         public IEnumerable<Employee> Employees { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             Employees = (await EmployeeService.GetEmployees()).ToList();
         }
+        protected int SelectedEmployeesCount { get; set; } = 0;
 
-       /*private void LoadEmployees()
+        protected void EmployeeSelectionChanged(bool isSelected)
         {
-            System.Threading.Thread.Sleep(2000);
-            Employee e1 = new Employee
+            if (isSelected)
             {
-                EmployeeId = 1,
-                FirstName = "John",
-                LastName = "Hastings",
-                Email = "David@pragimtech.com",
-                DateOfBrith = new DateTime(1980, 10, 5),
-                Gender = Gender.Male,
-                DepartmentId = 1,
-                PhotoPath = "images/john.png"
-            };
-
-            Employee e2 = new Employee
+                SelectedEmployeesCount++;
+            }
+            else
             {
-                EmployeeId = 2,
-                FirstName = "Sam",
-                LastName = "Galloway",
-                Email = "Sam@pragimtech.com",
-                DateOfBrith = new DateTime(1981, 12, 22),
-                Gender = Gender.Male,
-                DepartmentId = 2,
-                PhotoPath = "images/sam.jpg"
-            };
-
-            Employee e3 = new Employee
-            {
-                EmployeeId = 3,
-                FirstName = "Mary",
-                LastName = "Smith",
-                Email = "mary@pragimtech.com",
-                DateOfBrith = new DateTime(1979, 11, 11),
-                Gender = Gender.Female,
-                DepartmentId = 3,
-                PhotoPath = "images/mary.png"
-            };
-
-            Employee e4 = new Employee
-            {
-                EmployeeId = 3,
-                FirstName = "Sara",
-                LastName = "Longway",
-                Email = "sara@pragimtech.com",
-                DateOfBrith = new DateTime(1982, 9, 23),
-                Gender = Gender.Female,
-                DepartmentId = 4,
-                PhotoPath = "images/sara.png"
-            };
-
-            Employees = new List<Employee> { e1, e2, e3, e4 };
+                SelectedEmployeesCount--;
+            }
         }
-       */
+
+        /*private void LoadEmployees()
+         {
+             System.Threading.Thread.Sleep(2000);
+             Employee e1 = new Employee
+             {
+                 EmployeeId = 1,
+                 FirstName = "John",
+                 LastName = "Hastings",
+                 Email = "David@pragimtech.com",
+                 DateOfBrith = new DateTime(1980, 10, 5),
+                 Gender = Gender.Male,
+                 DepartmentId = 1,
+                 PhotoPath = "images/john.png"
+             };
+
+             Employee e2 = new Employee
+             {
+                 EmployeeId = 2,
+                 FirstName = "Sam",
+                 LastName = "Galloway",
+                 Email = "Sam@pragimtech.com",
+                 DateOfBrith = new DateTime(1981, 12, 22),
+                 Gender = Gender.Male,
+                 DepartmentId = 2,
+                 PhotoPath = "images/sam.jpg"
+             };
+
+             Employee e3 = new Employee
+             {
+                 EmployeeId = 3,
+                 FirstName = "Mary",
+                 LastName = "Smith",
+                 Email = "mary@pragimtech.com",
+                 DateOfBrith = new DateTime(1979, 11, 11),
+                 Gender = Gender.Female,
+                 DepartmentId = 3,
+                 PhotoPath = "images/mary.png"
+             };
+
+             Employee e4 = new Employee
+             {
+                 EmployeeId = 3,
+                 FirstName = "Sara",
+                 LastName = "Longway",
+                 Email = "sara@pragimtech.com",
+                 DateOfBrith = new DateTime(1982, 9, 23),
+                 Gender = Gender.Female,
+                 DepartmentId = 4,
+                 PhotoPath = "images/sara.png"
+             };
+
+             Employees = new List<Employee> { e1, e2, e3, e4 };
+         }
+        */
     }
 }
